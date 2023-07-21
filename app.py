e7e19db260ac8e4b4d19cce83078efc2844514df
2020Priyanshu <2020priyanshu@gmail.com>
2023-07-21 11:34:00 +0530

    Created embeddings

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 from langchain.embeddings import TensorflowHubEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain import OpenAI
 from langchain import HuggingFaceHub
@@ -11,8 +12,17 @@ from langchain import HuggingFaceHub
 from flask_cors import CORS
 from dotenv import load_dotenv
 
+import chromadb
+from chromadb.config import Settings
+from chromadb.utils import embedding_functions
+
 app = Flask(__name__)
 CORS(app, origins='*')
 
 print("loading environment variables")
 load_dotenv()
+
+print("loading chromadb emeddings")
+chromadb_embeddings = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+print("loading langchain emeddings")
+langchain_embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
