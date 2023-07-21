e546dd13f0912f598d469c4cfc5cad8dec67d1c8
Zad-100 <akash2001parida@gmail.com>
2023-07-21 12:39:38 +0530

    Added the csv file - data based on which the chatbot has to respond.

--- app.py
+++ app.py
@@ -26,3 +26,72 @@ print("loading chromadb emeddings")
 chromadb_embeddings = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 print("loading langchain emeddings")
 langchain_embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+class user_Session():
+    def __init__(self):
+        self.selected_llm ="OpenAI"
+        self.query =""
+        self.sources =[]
+        self.chat_history = []
+        self.responses=[]
+        self.source_ids=[]
+        self.source_cites=[]
+        self.rating=5
+
+    def get_llm(self):
+        
+        llm = OpenAI()
+        return llm
+
+
+    def response_from_llm(self):
+        llm = self.get_llm()
+        self.response =""
+        self.source_id = ""
+        self.source_cites = []
+        self.chat_history=[]
+        
+        
+        qa = ConversationalRetrievalChain.from_llm(llm = llm, retriever = self.vectordb.as_retriever(), return_source_documents = True)
+
+        #get the response
+        result = qa({'question':self.query, "chat_history":self.chat_history})
+        self.response = result['answer']
+        target_document = result['source_documents'][0]
+        print(target_document.metadata['source'])
+        #self.source_cites.append(target_document.metadata['url'])
+        self.source_id = self.collection.query(query_texts=target_document.page_content, n_results=1)['ids'][0][0]
+        print(len(self.vectordb.get(where={"rating": {'$gt': 4}})['ids']))
+
+        for source in self.sources:
+            document = self.vectordb.similarity_search(self.query,k=1 , filter={
+                'source':source
+            })
+            self.source_cites.append(document[0].metadata['url'])
+        
+        return self.response, self.source_cites
+    
+    def update_rating(self):
+        # get the document with ids == self.source_ids[0] :: this returns a dictionary
+        doc = self.vectordb.get(ids=[self.source_id])
+        
+        # get the current rating of the article
+        curr_rating =doc['metadatas'][0]['rating']
+        # update the rating
+        new_rating = (curr_rating+self.rating)/2
+        doc['metadatas'][0]['rating'] = new_rating
+        
+        # create a langchain document from dictionary
+        document =Document(
+            ids=doc['ids'][0],
+            page_content=doc['documents'][0],
+            metadata={
+                'source':doc['metadatas'][0]['source'],
+                'url':doc['metadatas'][0]['url'],
+                'disease':doc['metadatas'][0]['disease'],
+                'rating':doc['metadatas'][0]['rating'],
+            }
+        )
+
+        # update the vectordb
+        vectordb.update_document(self.source_id,document)
