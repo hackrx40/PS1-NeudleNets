50fc45c6880d8af623dd434eda6ea2fa21a65454
2020Priyanshu <2020priyanshu@gmail.com>
2023-07-21 11:02:18 +0530

    Importing Modules

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 # backend for Medibot
+# import required packages
+from flask import Flask, jsonify, request
+from langchain.embeddings import TensorflowHubEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores.chroma import Chroma
+from langchain import OpenAI
+from langchain import HuggingFaceHub
