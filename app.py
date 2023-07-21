fc5e418a10a4f540a2448a8037f20269c2ea5168
2020Priyanshu <2020priyanshu@gmail.com>
2023-07-21 11:11:54 +0530

    Loading Environment Variables

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores.chroma import Chroma
 from langchain import OpenAI
 from langchain import HuggingFaceHub
+
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app, origins='*')
+
+print("loading environment variables")
+load_dotenv()
